8a11f4214c45b2ac6953d5d559aa23f50bda639b
Promie Yutasane <promie.yutasane@gmail.com>
2024-08-01 16:20:35 +1000

    CL-05 createLambda helper function

--- lib/cloud-lingo-stack.ts
+++ lib/cloud-lingo-stack.ts
@@ -1,8 +1,18 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 
 export class CloudLingoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+  }
+
+  createLambda = (name: string, path: string) => {
+    return new NodejsFunction(this, name, {
+      functionName: name,
+      runtime: Runtime.NODEJS_20_X,
+      entry: path,
+    })
   }
 }
