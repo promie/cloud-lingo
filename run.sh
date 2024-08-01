98b6484decbd38361ae340e7881f101aec33a1c8
Promie Yutasane <promie.yutasane@gmail.com>
2024-08-01 15:58:01 +1000

    CL-03 setting up prettier, eslint and husky

--- run.sh
+++ run.sh
@@ -3,3 +3,11 @@ $ cdk bootstrap aws://ACCOUNT-NUMBER/REGION
 
 # Create a new CDK project
 $ cdk init app --language typescript
+
+# ESLint and Prettier packages
+npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-config-standard-with-typescript eslint-plugin-import eslint-plugin-n eslint-plugin-promise prettier
+
+# Husky and lint-staged packages
+npm i -D husky lint-staged
+
+export AWS_PROFILE=promie
