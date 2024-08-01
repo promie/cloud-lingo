618b60803c2e642ec535a6cd68721f041b531e75
Promie Yutasane <promie.yutasane@gmail.com>
2024-08-02 08:12:46 +1000

    CL-07 destructure the body to dynamically translate language

--- src/postTranslation.ts
+++ src/postTranslation.ts
@@ -1,15 +1,32 @@
-import { APIGatewayProxyResult } from 'aws-lambda'
-import { TranslateTextCommand } from '@aws-sdk/client-translate'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  TranslateTextCommand,
+  TranslateTextCommandOutput,
+} from '@aws-sdk/client-translate'
 import { AWSTranslateClient } from './lib/translate'
 
-export const handler = async (): Promise<APIGatewayProxyResult> => {
+interface IPostTranslationRequest {
+  sourceLang: string
+  targetLang: string
+  text: string
+}
+
+export const handler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  const { sourceLang, targetLang, text } = JSON.parse(
+    event.body!,
+  ) as IPostTranslationRequest
+
   const command = new TranslateTextCommand({
-    SourceLanguageCode: 'en',
-    TargetLanguageCode: 'th',
-    Text: 'Hey, how are you going?',
+    SourceLanguageCode: sourceLang,
+    TargetLanguageCode: targetLang,
+    Text: text,
   })
 
-  const translation: any = await AWSTranslateClient.send(command as any)
+  const translation: TranslateTextCommandOutput = await AWSTranslateClient.send(
+    command as any,
+  )
 
   return {
     statusCode: 200,
