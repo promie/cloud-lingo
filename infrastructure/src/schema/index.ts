af9cfe0d8769344e544d619709d10a0251a8cbb2
Promie Yutasane <promie.yutasane@gmail.com>
2024-08-05 06:12:35 +1000

    CL-23 complete moving lambdas into its own package

--- infrastructure/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod'
-
-export const translationRequestSchema = z.object({
-  sourceLang: z.string(),
-  targetLang: z.string(),
-  sourceText: z.string(),
-})
--- infrastructure/src/utils/headers.ts
+++
@@ -1,4 +0,0 @@
-export const defaultHeaders = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-}
