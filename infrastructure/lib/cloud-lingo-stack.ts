2ed7999429eabe7b90680f1e030dbf870fbe9f69
Promie Yutasane <promie.yutasane@gmail.com>
2024-08-06 05:17:55 +1000

    CL-27 define the dynamoDB construct and grant read/write access to lambda function

--- infrastructure/lib/cloud-lingo-stack.ts
+++ infrastructure/lib/cloud-lingo-stack.ts
@@ -1,8 +1,9 @@
 import * as cdk from 'aws-cdk-lib'
 import * as path from 'path'
-import { Cors, RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway'
+import { Cors, LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 import { Construct } from 'constructs'
 import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
 
@@ -12,6 +13,16 @@ export class CloudLingoStack extends cdk.Stack {
 
     const projectRoot = '../'
     const lambdaDirPath = path.join(projectRoot, 'packages/lambdas')
+
+    // DynamoDB construct goes here
+    const table = new Table(this, 'translations', {
+      tableName: 'translations',
+      partitionKey: {
+        name: 'requestId',
+        type: AttributeType.STRING,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
 
     // API Gateway
     const api = new RestApi(this, 'cloudLingoApi', {
@@ -33,6 +44,9 @@ export class CloudLingoStack extends cdk.Stack {
       'translateLambda',
       translateLambdaPath,
     )
+
+    // Grant the lambda function read/write permissions to the DynamoDB table
+    table.grantReadWriteData(postTranslationFunction)
 
     // A policy that gets attached to the lambda function allowing it to use the Translate service
     const translateAccessPolicy = new PolicyStatement({
