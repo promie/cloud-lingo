9932c6714a0061885c31ff60872c29a0eaee051b
Promie Yutasane <promie.yutasane@gmail.com>
2024-09-01 07:24:05 +1000

    CL-61 usage of config in app and cdk stack

--- infrastructure/src/app.ts
+++ infrastructure/src/app.ts
@@ -2,11 +2,14 @@
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { TranslatorServiceStack } from './stacks'
+import { getConfig } from './helpers'
+
+const config = getConfig()
 
 const app = new cdk.App()
 new TranslatorServiceStack(app, 'TranslatorService', {
   env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: 'us-east-1',
+    account: config.awsAccountId,
+    region: config.awsRegion,
   },
 })
