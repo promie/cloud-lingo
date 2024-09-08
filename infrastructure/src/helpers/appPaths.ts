8d4a6ddb39a266c215608b09a55c5b2c50fc0271
Promie Yutasane <promie.yutasane@gmail.com>
2024-09-09 06:17:01 +1000

    CL-63 constructed the appPaths helper for projectRoot and lambdaDirPath

---
+++ infrastructure/src/helpers/appPaths.ts
@@ -0,0 +1,22 @@
+import * as path from 'path'
+import * as fs from 'fs'
+
+const findRootEnv = (searchPath: string): string => {
+  if (searchPath === '/') {
+    throw new Error('.env file not found')
+  }
+
+  if (fs.readdirSync(searchPath).find(file => file === 'env')) {
+    return searchPath
+  }
+
+  return findRootEnv(path.join(searchPath, '../'))
+}
+
+export const projectRootPath = findRootEnv(__dirname)
+export const lambdaDirPath = path.join(projectRootPath, 'packages/lambdas')
+export const lambdaLayersDirPath = path.join(
+  projectRootPath,
+  'packages/lambda-layers',
+)
+export const frontendDistPath = path.join(projectRootPath, 'apps/frontend/dist')
